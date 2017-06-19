91782e767f9d288eec37a8a8ae218bc5ddb1f8f1
Rahul Madhavan <rahul.maddy@gmail.com>
2017-06-19 13:07:46 +0530

    model created

--- pollsite/polls/urls.py
+++ pollsite/polls/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='poll_index'),
+    url(r'^([0-9]+)/$', views.detail, name='poll_detail'),
+    # or you can pass the regex with a name as follows
+    url(r'^(?P<question_id>[0-9]+)/results$', views.results, name='poll_results'),
+    url(r'^([0-9]+)/vote$', views.vote, name='poll_vote'),
 ]
